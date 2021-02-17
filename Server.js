1a2a86c428a3813fb6d040cdab5432f681765256
Yash Mukund Purohit <purohityash03@gmail.com>
2021-02-17 18:00:01 +0530

    initial

---
+++ Server.js
@@ -0,0 +1,28 @@
+const  express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+const path = require('path')
+
+
+//create a server
+const app = express();
+const Port = process.env.Port || 3000 ; 
+
+app.get('/' , (req , res) => {
+    res.render('home')
+})
+// set template engine
+
+app.use(expressEjsLayouts);
+app.set('views' , path.join(__dirname ,'/resources/views'))
+app.set('view engine' , 'ejs')
+
+
+
+
+
+
+
+app.listen(Port,() =>{
+    console.log(`Server is listening to port number ${Port}`);
+})
