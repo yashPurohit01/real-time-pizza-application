87f943bb3dec2da2d48a02cacaec7e1d4cabdf52
SternritterFarid <relampagolanzadel04@gmail.com>
2021-02-21 15:12:55 +0530

    Added dotenv, session.

--- Server.js
+++ Server.js
@@ -1,29 +1,43 @@
-const  express = require('express');
-const expressEjsLayouts = require('express-ejs-layouts');
-const ejs = require('ejs');
 const path = require('path')
+const express = require('express')
+const expressEjsLayouts = require('express-ejs-layouts')
+const session = require('express-session')
+const dotenv = require('dotenv')
 
+// configure env
+dotenv.config()
 
-//create a server
-const app = express();
-const Port = process.env.Port || 3000 ; 
+// create a server
+const app = express()
 
-app.use(express.static('public'));
-app.get('/' , (req , res) => {
-    res.render('home')
-})
+const PORT = process.env.Port || 3000
+
+// set static folder -- for assets
+app.use(express.static('public'))
+
+// app.use(expressEjsLayouts)
+
+// set views directory
+app.set('views', path.join(__dirname, '/resources/views'))
+
 // set template engine
+app.set('view engine', 'ejs')
 
-app.use(expressEjsLayouts);
-app.set('views' , path.join(__dirname ,'/resources/views'))
-app.set('view engine' , 'ejs')
+// add session middleware
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  })
+)
 
+// routes
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
-
-
-
-
-
-app.listen(Port,() =>{
-    console.log(`Server is listening to port number ${Port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening to port number ${PORT}`)
 })
